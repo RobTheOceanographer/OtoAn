63ae667d99c26cf810701c733a188ef28cf59a9d
Rob Johnson <robtheoceanographer@gmail.com>
2014-12-07 16:14:21 +1100

    moved the loading of worksheets to the front which makes the plotting loop much faster. Prototype complete

--- otoAnAPP/proofOfConcept.r
+++ otoAnAPP/proofOfConcept.r
@@ -8,6 +8,7 @@ library("XLConnect")
 
 filename <- "/home/dev/projects/otoAn/exampleInputData/BLOCK 33.xls"
 # Read the workbook
+message("***** Loading in the data. This could take a while! *****")
 wb_object<-loadWorkbook(filename)
 
 # Save each sheet's name as a vector
@@ -18,19 +19,26 @@ tab_indexStart <- which(wb_sheets == tabRefName) +1
 
 # extract a sheet into a data frame.
 #dat <- readWorksheet(wbObject, sheet = getSheets(wbObject))
-tab_indexList = tab_indexStart:length(wb_sheets)
+tab_indexList <- tab_indexStart:length(wb_sheets)
 
-for (tab_index in tab_indexList) {
-  cat(paste("..", (tab_index - tab_indexStart)+1 , ".."))
-  tabDat <- readWorksheet(wb_object, sheet = wb_sheets[tab_index], header = TRUE)
-  
-  plot(tabDat$Time..sec., tabDat$BS.Ca43, type="l", lwd=4)
-  title(wb_sheets[tab_index])
+message("LOADING ALL THE SHEETS AFTER THE CALIBRATION WORKSHEET...")
+dat<-lapply(seq_along(wb_sheets[tab_indexList]),function(i) readWorksheet(wb_object,sheet=wb_sheets[tab_indexList][i]))
+
+
+message("***** Data loaded, starting plotting now *****")
+savenameIDX = 1
+for (tabDat in dat) {
+  # cat(paste("..", atab , ".."))
+#  tabDat <- readWorksheet(wb_object, sheet = wb_sheets[tab_index], header = TRUE)
+  message("***** Select start then end point and click finish *****")
+  plot(tabDat$Time..sec., tabDat$BS.Ca43, type="l", lwd=3)
+  title(wb_sheets[tab_indexList][savenameIDX])
   xy <- identify(tabDat$Time..sec., tabDat$BS.Ca43)
+  dev.off()
   # lines(tabDat$Time..sec.[xy[1]:xy[2]],tabDat$BS.Ca43[xy[1]:xy[2]],col="red",lwd=6)
 
   # chop the columns that contain 'mol' (that's the grep bit) based on user input (that's th exy stuff) indices.
-
+  message("***** SAVING THE TRIMMED DATA *****")
   subTabDat_molCols <- tabDat[xy[1]:xy[2],grep("mol", colnames(tabDat))]
   subTabDat_molCols$time <- tabDat$Time..sec.[xy[1]:xy[2]]
   
@@ -45,8 +53,13 @@ for (tab_index in tab_indexList) {
   
   # plot(subTabDat_molCols$time, subTabDat_molCols$Li7_Ca43_umol_mol,type='l')
   #savename = gsub(" ", "", paste("TRIMMED_", basename(filename), ".csv"))
-  savename = gsub(" ", "", paste(filename,"_TRIMMED_",wb_sheets[tab_index],".csv"))
+  savename = gsub(" ", "", paste(filename,"_TRIMMED_",wb_sheets[tab_indexList][savenameIDX],".csv"))
+  savenameIDX <-savenameIDX + 1
   write.csv(subTabDat_molCols, file = savename,row.names=FALSE)
+  rm(subTabDat_molCols)
 }
 
+message("Cleaning up...")
+rm(list=ls()) 
+message("***** FINISHED *****")
 
